b224f3e626fd3c56e9cd75d77017af13c0569346
petSzathmary <peter.szathmary@akademiasovy.sk>
2017-10-24 20:14:08 +0200

    1.commit

---
+++ randomECV.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<random>
+#include<ctime>
+using namespace std;
+int main()
+{
+	mt19937 randGen(time(0));
+	uniform_int_distribution<int> ranType2(0, 2);
+	uniform_int_distribution<int> ran0_9(48, 57);
+	int i, type;
+	char ecv[3];
+	cout << "Random ECV of a KE car: KE-";
+	for (i = 0; i < 3; i++)
+	{
+		type = ranType2(randGen);
+		switch (type)
+		{
+		case 0: ecv[i] = ran0_9(randGen);
+			break;
+		case 1: ecv[i] = ran0_9(randGen);
+			break;
+		case 2: ecv[i] = ran0_9(randGen);
+			break;
+		}
+	}
+	for (i = 0; i < 3; i++)
+	{
+		cout << ecv[i];
+	}
+	cout << endl;
+	return 0;
+}
